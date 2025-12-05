fc5f9298592007af93aace348945e0384f38b060
Mike <malekseenko4444@gmail.com>
2025-12-05 15:57:25 +0200

    continue to tinker

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -3,19 +3,10 @@ local function enable_transparency()
 end
 
 return { 
-    {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-	    vim.cmd.colorscheme "catppuccin"
+	    vim.cmd.colorscheme "catppuccin-mocha"
 	end
-    },
-    {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	opts = {
-	    theme = 'catppuccin',
-	}
-    }
 }
